0c495ce5cb96bc305e631b9e38f26014b2ef7e9f
José Luis Elustondo <joselus62@yahoo.com.ar>
2022-11-07 18:04:01 -0300

    Style Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
+import './navbar.css';
+
 
 export default function NavBar() {
     return (
         <>
-            <nav style={{ display: "flex", justifyContent: "space-around" }}>
+            <div className="App-navbar">
+            
+            <nav style={{ display: "flex", justifyContent: "space-around", }}>
                 <Link to="/Inicio">Inicio</Link>
+                <Link to="/Profile">Perfil</Link>
                 <Link to="/Libros">Lista de Libros</Link>
-                <Link to="/Autor">Autores</Link>
+                <Link to="/Autor">Autores</Link>                
                 <Link to="/Categoría">Categorías</Link>
                 <Link to="/"></Link>
             </nav>
+            </div>
         </>
     );
 }
