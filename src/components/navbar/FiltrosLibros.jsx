ccc8c1c0e206c24eab53b1d17a35d4bb556f6b34
rosseab-bit <roseab.developer@gmail.com>
2022-11-15 18:19:35 -0300

    modifico card de categorias

--- src/components/navbar/FiltrosLibros.jsx
+++ src/components/navbar/FiltrosLibros.jsx
@@ -4,9 +4,17 @@ import ListGroup from 'react-bootstrap/ListGroup';
 const FiltrosLibros = () => {
     return (
         <>
-            <Card style={{ width: '15rem' }}>
+            <Card style={{ width: '15rem'}}>
                 <ListGroup variant="flush">
-                    <ListGroup.Item style={{borderRadius:10}}>Terror</ListGroup.Item>
+                    <ListGroup.Item 
+                    style={{
+                        borderRadius:10, 
+                        backgroundColor: "#AEBDCA",
+                        textAlign: "center"
+                    }}
+                        >Categoria
+                    </ListGroup.Item>
+                    <ListGroup.Item>Terror</ListGroup.Item>
                     <ListGroup.Item>Drama</ListGroup.Item>
                     <ListGroup.Item>Ciencia Ficcion</ListGroup.Item>
                     <ListGroup.Item>Historia</ListGroup.Item>
