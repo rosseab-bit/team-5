bed79fc51eb50e9a47264973dee6af51a60904d3
hunter <hunter@hunter.hunter.xyx>
2022-11-27 11:15:37 -0300

    agrego footer, todavia falta pulir diseño

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,20 +1,96 @@
+import { RiHome2Line } from 'react-icons/ri'
+import { FaAngleRight } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaTelegramPlane } from 'react-icons/fa'
+import { FaAt } from 'react-icons/fa'
+import { IoLogoFacebook } from 'react-icons/io5'
+import FooterImage from './statics/img/bannerFooter.jpg'
+
+
 const FooterComponent = () => {
     const year = new Date().getFullYear();
     return (
         <>
             <div
-            style={{
-                left: 0,
-                bottom: 0,
-                width: '100%',
-                backgroundColor: '#7895B2',
-                color: 'black',
-                textAlign: 'center'
-            }}
+                style={{
+                    left: 0,
+                    bottom: 0,
+                    width: '100%',
+                    backgroundColor: '#7895B2',
+                    color: 'black',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-around'
+                }}
             >
-                <p>
-                    {`Copyright © Upbeat Code ${year}`}
-                </p>
+                <div
+                style={{
+                    marginTop: 5
+                }}
+                >
+                    <p><RiHome2Line /> Sobre nosotros</p>
+                    <ul
+                        style={{
+                            listStyle: 'none'
+                        }}
+                    >
+                        <li><FaAngleRight /> Grupo 5</li>
+                        <li><FaAngleRight /> Curso codo a codo 2022</li>
+                        <li><FaAngleRight /> React JS</li>
+                        <li><FaAngleRight /> App de libros</li>
+                    </ul>
+                </div>
+                <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 5
+                }}
+                >
+                    
+                    <img src={FooterImage}
+                        width='150px'
+                        height='auto'
+                        border='2'
+                        style={{
+                            'object-fit': 'cover',
+                            'border-radius': '50%',
+                            'height': '150px'
+                        }}
+                    />
+                    <p
+                    >
+                        {`Copyright © Upbeat Code ${year}`}
+                    </p>
+                </div>
+                <div
+                style={{
+                    margionTop: 5
+                }}
+                >
+                    <p>Nos puedes encontrar en:</p>
+                    <ul
+                        style={{
+                            listStyle: 'none'
+                        }}
+                    >
+                        <li>
+                            <FaInstagram /> Instagram
+                        </li>
+                        <li>
+                            <FaTelegramPlane /> Telegram
+                        </li>
+                        <li>
+                            <FaAt /> gigilibros@mail.com
+                        </li>
+                        <li>
+                            <IoLogoFacebook /> Facebook
+                        </li>
+                    </ul>
+                </div>
             </div>
         </>
     );
